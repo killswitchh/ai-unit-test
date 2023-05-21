730ad68d28f4b6ba6cd47494df3dc404a2afbeda
GitHub Action <action@github.com>
2023-05-21 08:23:02 +0000

    [ACTIONS AI] Added Unit Tests

--- ai-tests/addition_ai_tests.py
+++ ai-tests/addition_ai_tests.py
@@ -7,10 +7,8 @@ def test_math_functions():
     assert add(2, 3) == 5
     assert add(-2, 3) == 1
     assert add(0, 0) == 0
-    assert subtract(5, 2) == 3
-    assert subtract(-5, -2) == -3
     assert subtract(5, 3) == 2
-    assert subtract(-5, 3) == -8
+    assert subtract(3, 5) == -2
     assert subtract(0, 0) == 0
 
 test_math_functions()
--- ai-tests/multiplication_ai_tests.py
+++ ai-tests/multiplication_ai_tests.py
@@ -5,9 +5,9 @@ from multiplication import multiply
 
 def test_multiply():
     assert multiply(2, 3) == 6
-    assert multiply(0, 4) == 0
-    assert multiply(-2, 5) == -10
+    assert multiply(0, 5) == 0
+    assert multiply(-4, 8) == -32
     assert multiply(2.5, 4) == 10.0
-    assert multiply(2, -3.5) == -7.0
+    assert multiply(2, -3) == -6
 
 test_multiply()
